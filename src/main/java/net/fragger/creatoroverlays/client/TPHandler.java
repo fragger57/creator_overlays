b3ce8cef175c45472fd6f414bc14df5f3b5e4ff1
fragger57 <fraggerdagger57@gmail.com>
2023-12-01 13:04:48 -0500

    Tracking points are now stored in an Array List

--- src/main/java/net/fragger/creatoroverlays/client/TPHandler.java
+++ src/main/java/net/fragger/creatoroverlays/client/TPHandler.java
@@ -1,188 +1,188 @@
 package net.fragger.creatoroverlays.client;
 
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fragger.creatoroverlays.client.overlays.TPOverlay;
+import net.fragger.creatoroverlays.client.gui.overlayguis.TPGUIRow;
+import net.minecraft.text.Text;
 
-import static net.fragger.creatoroverlays.client.GUI.TrackingPointsGUI.*;
-import static net.fragger.creatoroverlays.event.KeyInputHandler.tpGUI;
+import java.util.ArrayList;
+
+import static net.fragger.creatoroverlays.client.gui.RootGUI.tpGUI;
+import static net.fragger.creatoroverlays.client.gui.overlayguis.TPGUI.toggleAll;
 
 public class TPHandler {
 
-    public static TPOverlay tp1 = new TPOverlay();
-    public static TPOverlay tp2 = new TPOverlay();
-    public static TPOverlay tp3 = new TPOverlay();
-    public static TPOverlay tp4 = new TPOverlay();
+    public static ArrayList<TPGUIRow> tpGUIRows = new ArrayList<TPGUIRow>();
+    public static int tpArraySize = 0;
+    public static final int MAX_ARRAY_SIZE = 16;
 
     private static boolean isRendered = false;
 
+    public void addTP() {
+        if (tpGUIRows.size() < MAX_ARRAY_SIZE) {
+            tpGUI.addRow(tpArraySize);
+            tpGUIRows.get(tpArraySize).initilize();
+            tpArraySize++;
+            toggleAllOn();
+        }
+    }
+
     public void updateRenderStatus() {
         if (isRendered) {
-            row1.toggleOff();
-            row2.toggleOff();
-            row3.toggleOff();
-            row4.toggleOff();
-            toggleAll.setToggle(false);
+            toggleAllOff();
         } else {
-            tp1.setIsRendered(tp1.renderStatus());
-            tp2.setIsRendered(tp2.renderStatus());
-            tp3.setIsRendered(tp3.renderStatus());
-            tp4.setIsRendered(tp4.renderStatus());
-
-            if(!tp1.isRendered() & !tp2.isRendered() & !tp3.isRendered() & !tp4.isRendered()) {
-                row1.toggleOn();
-                row2.toggleOn();
-                row3.toggleOn();
-                row4.toggleOn();
-            }
-            if (tp1.isRendered()) {
-                row1.toggleOn();
-            }
-            if (tp2.isRendered()) {
-                row2.toggleOn();
-            }
-            if (tp3.isRendered()) {
-                row3.toggleOn();
-            }
-            if (tp4.isRendered()) {
-                row4.toggleOn();
-            }
-            toggleAll.setToggle(true);
+            toggleAllOn();
         }
-        isRendered = !isRendered;
     }
-    public void toggleOn() {
-        if(!isRendered) {
-            tp1.setIsRendered(tp1.renderStatus());
-            tp2.setIsRendered(tp2.renderStatus());
-            tp3.setIsRendered(tp3.renderStatus());
-            tp4.setIsRendered(tp4.renderStatus());
 
-            if (!tp1.isRendered() & !tp2.isRendered() & !tp3.isRendered() & !tp4.isRendered()) {
-                row1.toggleOn();
-                row2.toggleOn();
-                row3.toggleOn();
-                row4.toggleOn();
+    public void updateArray() {
+        tpArraySize = 0;
+        ArrayList<TPGUIRow> tempArray = new ArrayList<TPGUIRow>();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            if (tpGUIRow != null) {
+                tpGUIRow.updateLabel(Text.of("Tracking Point " + (tpArraySize + 1)));
+                tempArray.add(tpGUIRow);
+                tpArraySize++;
             }
-            if (tp1.isRendered()) {
-                row1.toggleOn();
+        }
+        tpGUIRows = tempArray;
+    }
+
+    public void toggleAllOn() {
+        if (tpGUIRows.size() != 0) {
+            int renderTotal = 0;
+            for (int i = 0; i < tpArraySize; i++) {
+                if (!tpGUIRows.get(i).getTP().invisible()) {
+                    tpGUIRows.get(i).toggleOn();
+                    renderTotal++;
+                }
             }
-            if (tp2.isRendered()) {
-                row2.toggleOn();
-            }
-            if (tp3.isRendered()) {
-                row3.toggleOn();
-            }
-            if (tp4.isRendered()) {
-                row4.toggleOn();
+            if (renderTotal == 0) {
+                for (int i = 0; i < tpArraySize; i++) {
+                    tpGUIRows.get(i).toggleOn();
+                    tpGUIRows.get(i).getTP().setInvisible(false);
+                }
             }
             toggleAll.setToggle(true);
             isRendered = true;
         }
     }
-    public void initializePosition() {
-        tp1.initializeTPOverlay();
-        tp2.initializeTPOverlay();
-        tp3.initializeTPOverlay();
-        tp4.initializeTPOverlay();
-        row1.selectOn();
+
+    public void toggleAllOff() {
+        if (tpGUIRows.size() != 0) {
+            for (TPGUIRow tpGUIRow : tpGUIRows) {
+                tpGUIRow.toggleOff();
+            }
+        }
+        toggleAll.setToggle(false);
+        isRendered = false;
     }
-    public void initializeOverlay() {
-        HudRenderCallback.EVENT.register(tp1);
-        HudRenderCallback.EVENT.register(tp2);
-        HudRenderCallback.EVENT.register(tp3);
-        HudRenderCallback.EVENT.register(tp4);
-    }
+
     public void moveUp() {
-       tp1.moveUp();
-       tp2.moveUp();
-       tp3.moveUp();
-       tp4.moveUp();
+       for (TPGUIRow tpGUIRow : tpGUIRows) {
+           tpGUIRow.getTP().moveUp();
+       }
     }
     public void moveUpFast() {
-        tp1.moveUpFast();
-        tp2.moveUpFast();
-        tp3.moveUpFast();
-        tp4.moveUpFast();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            tpGUIRow.getTP().moveUpFast();
+        }
     }
     public void moveDown() {
-        tp1.moveDown();
-        tp2.moveDown();
-        tp3.moveDown();
-        tp4.moveDown();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            tpGUIRow.getTP().moveDown();
+        }
     }
     public void moveDownFast() {
-        tp1.moveDownFast();
-        tp2.moveDownFast();
-        tp3.moveDownFast();
-        tp4.moveDownFast();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            tpGUIRow.getTP().moveDownFast();
+        }
     }
     public void moveRight() {
-        tp1.moveRight();
-        tp2.moveRight();
-        tp3.moveRight();
-        tp4.moveRight();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            tpGUIRow.getTP().moveRight();
+        }
     }
     public void moveRightFast() {
-        tp1.moveRightFast();
-        tp2.moveRightFast();
-        tp3.moveRightFast();
-        tp4.moveRightFast();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            tpGUIRow.getTP().moveRightFast();
+        }
     }
     public void moveLeft() {
-        tp1.moveLeft();
-        tp2.moveLeft();
-        tp3.moveLeft();
-        tp4.moveLeft();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            tpGUIRow.getTP().moveLeft();
+        }
     }
     public void moveLeftFast() {
-        tp1.moveLeftFast();
-        tp2.moveLeftFast();
-        tp3.moveLeftFast();
-        tp4.moveLeftFast();
+        for (TPGUIRow tpGUIRow : tpGUIRows) {
+            tpGUIRow.getTP().moveLeftFast();
+        }
     }
+
     public boolean isRendered() {
         return isRendered;
     }
     public void cycleSelectedUp() {
-        if (tp1.isSelected()) {
-            tpGUI.updateSelected(row2);
-        } else if (tp2.isSelected()) {
-            tpGUI.updateSelected(row3);
-        } else if (tp3.isSelected()) {
-            tpGUI.updateSelected(row4);
-        } else if (tp4.isSelected()) {
-            tpGUI.updateSelected(row1);
-        } else {
-            tpGUI.updateSelected(row1);
+        if (tpArraySize > 1) {
+            for (int i = 0; i < tpArraySize; i++) {
+                if (tpGUIRows.get(i).getTP().isSelected()) {
+                    if (i == tpArraySize - 1) {
+                        tpGUIRows.get(i).deselect();
+                        tpGUIRows.get(0).select();
+                        if (!tpGUIRows.get(0).getTP().isRendered() && isRendered) {
+                            cycleSelectedUp();
+                        }
+                        return;
+                    } else {
+                        tpGUIRows.get(i).deselect();
+                        tpGUIRows.get(i + 1).select();
+                        if (!tpGUIRows.get(i + 1).getTP().isRendered() && isRendered) {
+                            cycleSelectedUp();
+                        }
+                        return;
+                    }
+                }
+            }
         }
     }
 
     public void cycleSelectedDown() {
-        if (tp1.isSelected()) {
-            tpGUI.updateSelected(row4);
-        } else if (tp2.isSelected()) {
-            tpGUI.updateSelected(row1);
-        } else if (tp3.isSelected()) {
-            tpGUI.updateSelected(row2);
-        } else if (tp4.isSelected()) {
-            tpGUI.updateSelected(row3);
-        } else {
-            tpGUI.updateSelected(row1);
+        if (tpArraySize > 1) {
+            for (int i = 0; i < tpArraySize; i++) {
+                if (tpGUIRows.get(i).getTP().isSelected()) {
+                    if (i == 0) {
+                        tpGUIRows.get(i).getTP().updateSelected();
+                        tpGUIRows.get(tpArraySize - 1).getTP().updateSelected();
+                        if (!tpGUIRows.get(tpArraySize - 1).getTP().isRendered() && isRendered) {
+                            cycleSelectedDown();
+                        }
+                        return;
+                    } else {
+                        tpGUIRows.get(i).getTP().updateSelected();
+                        tpGUIRows.get(i - 1).getTP().updateSelected();
+                        if (!tpGUIRows.get(i - 1).getTP().isRendered() && isRendered) {
+                            cycleSelectedDown();
+                        }
+                        return;
+                    }
+                }
+            }
         }
     }
     public void cycleColor() {
-        if (tp1.isSelected()) {
-            tp1.updateColor();
-            row1.updateColor();
-        } else if (tp2.isSelected()) {
-            tp2.updateColor();
-            row2.updateColor();
-        } else if (tp3.isSelected()) {
-            tp3.updateColor();
-            row3.updateColor();
-        } else if (tp4.isSelected()) {
-            tp4.updateColor();
-            row4.updateColor();
+        for (int i = 0; i < tpArraySize; i++) {
+            if (tpGUIRows.get(i).getTP().isSelected()) {
+                tpGUIRows.get(i).updateColor();
+                return;
+            }
+        }
+    }
+
+    public void updateSelected(TPGUIRow row) {
+        for (int i = 0; i < tpArraySize; i++) {
+            if (tpGUIRows.get(i) == row) {
+                tpGUIRows.get(i).select();
+            } else {
+                tpGUIRows.get(i).deselect();
+            }
         }
     }
 }
