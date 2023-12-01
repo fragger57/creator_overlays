3c1bc41a8d81a055809f00b0b9078a16b2c39dec
fragger57 <fraggerdagger57@gmail.com>
2023-12-01 13:02:00 -0500

    Key Input handling for new overlays

--- src/main/java/net/fragger/creatoroverlays/event/KeyInputHandler.java
+++ src/main/java/net/fragger/creatoroverlays/event/KeyInputHandler.java
@@ -3,18 +3,16 @@ package net.fragger.creatoroverlays.event;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fragger.creatoroverlays.client.GUI.TrackingPointsGUI;
-import net.fragger.creatoroverlays.client.GUI.TrackingPointsScreen;
 import net.fragger.creatoroverlays.client.TPHandler;
-import net.fragger.creatoroverlays.client.overlays.GROverlay;
-import net.fragger.creatoroverlays.client.overlays.RO3Overlay;
-import net.fragger.creatoroverlays.client.overlays.VVOverlay;
+import net.fragger.creatoroverlays.client.gui.COScreen;
+import net.fragger.creatoroverlays.client.gui.RootGUI;
+import net.fragger.creatoroverlays.client.gui.overlayguis.*;
+import net.fragger.creatoroverlays.client.overlays.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-import static net.fragger.creatoroverlays.util.StaticOverlay.*;
 import static net.fragger.creatoroverlays.client.overlays.VVOverlay.isGRVV;
 import static net.fragger.creatoroverlays.client.overlays.VVOverlay.isRO3VV;
 
@@ -24,6 +22,8 @@ public class KeyInputHandler {
     public static final String KEY_DISPLAY_GROVERLAY = "key.creatoroverlays.display_groverlay";
     public static final String KEY_DISPLAY_VVOVERLAY = "key.creatoroverlays.display_vvoverlay";
     public static final String KEY_DISPLAY_TPOVERLAY = "key.creatoroverlays.display_tpoverlay";
+    public static final String KEY_DISPLAY_CAMOVERLAY = "key.creatoroverlays.display_camoverlay";
+    public static final String KEY_DISPLAY_CUSTOMOVERLAY = "key.creatoroverlays.display_custom";
     public static final String KEY_CO_COLOR = "key.creatoroverlays.co_color";
     public static final String KEY_CO_ROTATEUP = "key.creatoroverlays.co_rotateup";
     public static final String KEY_CO_ROTATEDOWN = "key.creatoroverlays.co_rotatedown";
@@ -32,11 +32,14 @@ public class KeyInputHandler {
     public static final String KEY_CO_RIGHT = "key.creatoroverlays.co_right";
     public static final String KEY_CO_LEFT = "key.creatoroverlays.co_left";
     public static final String KEY_MODIFIER = "key.creatoroverlays.co_modifier";
+    public static final String KEY_GUI = "key.creatoroverlays.gui";
 
     public static KeyBinding ro3displayKey;
     public static KeyBinding grdisplayKey;
     public static KeyBinding vvdisplayKey;
     public static KeyBinding tpdisplayKey;
+    public static KeyBinding camdisplayKey;
+    public static KeyBinding customDisplayKey;
     public static KeyBinding cocolorKey;
     public static KeyBinding corotateupKey;
     public static KeyBinding corotatedownKey;
@@ -45,15 +48,21 @@ public class KeyInputHandler {
     public static KeyBinding corightKey;
     public static KeyBinding coleftKey;
     public static KeyBinding modifierKey;
+    public static KeyBinding guiKey;
 
-    public static RO3Overlay ro3Overlay = new RO3Overlay();
-    public static GROverlay grOverlay = new GROverlay();
-    public static VVOverlay vvOverlay = new VVOverlay();
+    public static RO3GUI ro3GUI = new RO3GUI();
+    public static RO3Overlay ro3Overlay = ro3GUI.getOverlay();
+    public static GRGUI grGUI = new GRGUI();
+    public static GROverlay grOverlay = grGUI.getOverlay();
+    public static VVGUI vvGUI = new VVGUI();
+    public static VVOverlay vvOverlay = vvGUI.getOverlay();
+    public static CAMGUI camGUI = new CAMGUI();
+    public static CamOverlay camOverlay = camGUI.getOverlay();
     public static TPHandler tpOverlay = new TPHandler();
-    public static TrackingPointsGUI tpGUI = new TrackingPointsGUI();
-    public static TrackingPointsScreen tpScreen = new TrackingPointsScreen(tpGUI);
-
-    private static int num = 0;
+    public static CustomGUI customGUI = new CustomGUI();
+    public static CustomOverlay customOverlay = customGUI.getOverlay();
+    public static RootGUI coGUI = new RootGUI();
+    public static COScreen coScreen = new COScreen(coGUI);
 
     public static void registerKeyInputs() {
 
@@ -66,42 +75,40 @@ public class KeyInputHandler {
             //Handles RO3 Overlay Key Press
             if (ro3displayKey.wasPressed()) {
                 if (!vvOverlay.isRendered()) {
-                    ro3Overlay.updateRenderStatus();
+                    ro3GUI.toggle();
                 } else {
                     if (isGRVV) {
                         isGRVV = false;
                     }
                     isRO3VV = !isRO3VV;
-                    HudRenderCallback.EVENT.register(vvOverlay);
                 }
             }
             //Handles Golden Ratio Key Press
             if (grdisplayKey.wasPressed()) {
                 if (!vvOverlay.isRendered()) {
-                    grOverlay.updateRenderStatus();
+                    grGUI.toggle();
                 } else {
                     if (isRO3VV) {
                         isRO3VV = false;
                     }
                     isGRVV = !isGRVV;
-                    HudRenderCallback.EVENT.register(vvOverlay);
                 }
             }
             //Handles Vertical Video Key Press
             if (vvdisplayKey.wasPressed()) {
-                vvOverlay.updateRenderStatus();
+                vvGUI.toggle();
             }
             //Handels Tracking Points Key Press
             if (tpdisplayKey.wasPressed()) {
-                if (num == 0) {
-                    num++;
-                    tpOverlay.initializePosition();
-                }
-                if (modifierKey.isPressed()) {
-                    MinecraftClient.getInstance().setScreen(tpScreen);
-                } else {
-                    tpOverlay.updateRenderStatus();
-                }
+                tpOverlay.updateRenderStatus();
+            }
+            //Handles Cam Key Press
+            if (camdisplayKey.wasPressed()) {
+                camGUI.toggle();
+            }
+            //Handles Custom Key Press
+            if (customDisplayKey.wasPressed()) {
+                customGUI.toggle();
             }
 
             /*
@@ -112,24 +119,42 @@ public class KeyInputHandler {
             if (cocolorKey.wasPressed()) {
                 if (tpOverlay.isRendered()) {
                     tpOverlay.cycleColor();
-                } else if (ro3Overlay.isRendered() || grOverlay.isRendered() || vvOverlay.isRendered()) {
-                    colorCycle();
+                } else if (camOverlay.isRendered()) {
+                    camGUI.changeColor();
+                } else if (ro3Overlay.isRendered()) {
+                    ro3GUI.changeColor();
+                } else if (grOverlay.isRendered()) {
+                    grGUI.changeColor();
+                } else if (vvOverlay.isRendered()){
+                    vvGUI.changeColor();
                 }
             }
             //Cycles Rotations Up
             if (corotateupKey.wasPressed()) {
                 if (tpOverlay.isRendered()) {
                     tpOverlay.cycleSelectedUp();
-                } else if (grOverlay.isRendered() || isGRVV) {
-                    rotateUp();
+                } else if (camOverlay.isRendered()) {
+                    camGUI.swapSides();
+                } else if (grOverlay.isRendered() || vvOverlay.getGRVV()) {
+                    grGUI.rotateUp();
+                } else if (ro3Overlay.isRendered()) {
+                    ro3GUI.changeGrid();
+                } else if (customOverlay.isRendered()) {
+                    customGUI.changeOverlayUp();
                 }
             }
             //Cycles Rotations Down
             if (corotatedownKey.wasPressed()) {
                 if (tpOverlay.isRendered()) {
                     tpOverlay.cycleSelectedDown();
-                } else if (grOverlay.isRendered() || isGRVV) {
-                    rotateDown();
+                } else if (camOverlay.isRendered()) {
+                    camGUI.swapSides();
+                } else if (grOverlay.isRendered() || vvOverlay.getGRVV()) {
+                    grGUI.rotateDown();
+                } else if (ro3Overlay.isRendered()) {
+                    ro3GUI.changeGrid();
+                } else if (customOverlay.isRendered()) {
+                    customGUI.changeOverlayDown();
                 }
             }
 
@@ -169,6 +194,10 @@ public class KeyInputHandler {
                     tpOverlay.moveLeftFast();
                 }
             }
+            //opens GUI
+            if (guiKey.wasPressed()) {
+                MinecraftClient.getInstance().setScreen(coScreen);
+            }
         });
     }
     public static void register() {
@@ -204,7 +233,21 @@ public class KeyInputHandler {
         tpdisplayKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 KEY_DISPLAY_TPOVERLAY,
                 InputUtil.Type.KEYSYM,
+                InputUtil.UNKNOWN_KEY.getCode(),
+                KEY_CATEGORY_CREATOROVERLAYS
+        ));
+        //sets Cam key bind
+        camdisplayKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_DISPLAY_CAMOVERLAY,
+                InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_F7,
+                KEY_CATEGORY_CREATOROVERLAYS
+        ));
+        //sets Custom key bind
+        customDisplayKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_DISPLAY_CUSTOMOVERLAY,
+                InputUtil.Type.KEYSYM,
+                InputUtil.UNKNOWN_KEY.getCode(),
                 KEY_CATEGORY_CREATOROVERLAYS
         ));
         //sets color cycle key bind
@@ -251,6 +294,12 @@ public class KeyInputHandler {
                 GLFW.GLFW_KEY_LEFT,
                 KEY_CATEGORY_CREATOROVERLAYS
         ));
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+           KEY_GUI,
+           InputUtil.Type.KEYSYM,
+           GLFW.GLFW_KEY_GRAVE_ACCENT,
+           KEY_CATEGORY_CREATOROVERLAYS
+        ));
 
         registerKeyInputs();
     }
@@ -258,6 +307,7 @@ public class KeyInputHandler {
         HudRenderCallback.EVENT.register(ro3Overlay);
         HudRenderCallback.EVENT.register(grOverlay);
         HudRenderCallback.EVENT.register(vvOverlay);
-        tpOverlay.initializeOverlay();
+        HudRenderCallback.EVENT.register(camOverlay);
+        HudRenderCallback.EVENT.register(customOverlay);
     }
 }
