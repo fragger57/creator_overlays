c4a66021a19d47033f9dc3150b771658f7dc12b9
fragger57 <fraggerdagger57@gmail.com>
2024-04-14 19:26:12 -0400

    Moved Project Files

---
+++ src/client/java/net/fragger/creatoroverlays/client/gui/overlayguis/CustomGUI.java
@@ -0,0 +1,111 @@
+package net.fragger.creatoroverlays.client.gui.overlayguis;
+
+import com.mojang.datafixers.util.Pair;
+import io.github.cottonmc.cotton.gui.widget.*;
+import io.github.cottonmc.cotton.gui.widget.data.HorizontalAlignment;
+import net.fragger.creatoroverlays.client.gui.AbstractGUI;
+import net.fragger.creatoroverlays.client.overlays.CustomOverlay;
+import net.fragger.creatoroverlays.util.config.COConfigs;
+import net.minecraft.text.Text;
+
+import static net.fragger.creatoroverlays.client.gui.RootGUI.*;
+import static net.fragger.creatoroverlays.event.KeyInputHandler.coScreen;
+import static net.fragger.creatoroverlays.util.config.COConfigs.CUSTOM_MODE;
+
+public class CustomGUI extends AbstractGUI {
+
+    public WPlainPanel customPanel = new WPlainPanel();
+
+    private WToggleButton toggle;
+    private WTextField filePath;
+    private WButton set;
+    private WButton cycleUp;
+    private WButton cycleDown;
+    private WSprite texture;
+
+    private static CustomOverlay overlay = new CustomOverlay();
+
+    public CustomGUI() {
+        overlay.setRotation(CUSTOM_MODE);
+
+        customPanel.setSize(guiWidth, guiHeight);
+
+        WLabel head = new WLabel(Text.translatable("key.creatoroverlays.gui.custom"));
+        head.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        customPanel.add(head, 120, 10);
+
+        toggle = new WToggleButton(TOGGLE_ON, TOGGLE_OFF);
+        customPanel.add(toggle, 10, y);
+        toggle.setOnToggle(on -> {
+            toggle();
+        });
+
+        /*filePath = new WTextField(Text.literal("File Path"));
+        filePath.setMaxLength(512);
+        customPanel.add(filePath, 38, y, 140, 18);
+
+        set = new WButton(Text.literal("Set"));
+        customPanel.add(set, 148, y, 30, 20);
+        set.setOnClick(() -> {
+           overlay.setTexture(filePath.getText());
+        });*/
+
+        cycleDown = new WButton(Text.of("<"));
+        customPanel.add(cycleDown, 190, y, 20, 20);
+        cycleDown.setOnClick(() -> {
+            changeOverlayDown();
+        });
+
+        cycleUp = new WButton(Text.of(">"));
+        customPanel.add(cycleUp, 220, y, 20, 20);
+        cycleUp.setOnClick(() -> {
+            changeOverlayUp();
+        });
+
+
+
+        WSprite outline = new WSprite(OUTLINE);
+        customPanel.add(outline, 10, y * 2, 230, 131);
+
+        updateSprite();
+
+        WButton close = new WButton(Text.translatable("key.creatoroverlays.gui.close"));
+        customPanel.add(close, 200, 200, 40, 20);
+        close.setOnClick(() -> {
+            coScreen.close();
+            COConfigs.writeConfigs();
+        });
+    }
+
+    public void updateSprite() {
+        customPanel.remove(texture);
+        texture = new WSprite(overlay.getTexture());
+        customPanel.add(texture, 12, y * 2 + 2, 226, 127);
+    }
+
+    public void toggle() {
+        overlay.updateRenderStatus();
+        toggle.setToggle(overlay.isRendered());
+    }
+
+    public void toggle(boolean display) {
+        overlay.setisRendered(display);
+        toggle.setToggle(display);
+    }
+
+    public void changeOverlayUp() {
+        overlay.rotateUp();
+        updateSprite();
+        COConfigs.updateConfig(new Pair<>("custom.mode", overlay.rotation));
+    }
+
+    public void changeOverlayDown() {
+        overlay.rotateDown();
+        updateSprite();
+        COConfigs.updateConfig(new Pair<>("custom.mode", overlay.rotation));
+    }
+
+    public CustomOverlay getOverlay() {
+        return overlay;
+    }
+}
---
+++ src/client/java/net/fragger/creatoroverlays/client/overlays/CustomOverlay.java
@@ -0,0 +1,57 @@
+package net.fragger.creatoroverlays.client.overlays;
+
+import com.mojang.datafixers.util.Pair;
+import net.fragger.creatoroverlays.client.Overlay;
+import net.fragger.creatoroverlays.client.StaticOverlay;
+import net.fragger.creatoroverlays.creatoroverlays;
+import net.fragger.creatoroverlays.util.config.COConfigs;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Identifier;
+
+public class CustomOverlay extends StaticOverlay implements Overlay {
+
+    private static Identifier Custom01 = new Identifier(creatoroverlays.MOD_ID,"textures/overlays/custom/custom01.png");
+    private static Identifier Custom02 = new Identifier(creatoroverlays.MOD_ID,"textures/overlays/custom/custom02.png");
+    private static Identifier Custom03 = new Identifier(creatoroverlays.MOD_ID,"textures/overlays/custom/custom03.png");
+    private static Identifier Custom04 = new Identifier(creatoroverlays.MOD_ID,"textures/overlays/custom/custom04.png");
+
+    private boolean isRendered = false;
+
+    @Override
+    public void onHudRender(DrawContext drawContext, float tickDelta) {
+        if (isRendered) {
+            render(drawContext, getTexture());
+        }
+    }
+
+    public void updateRenderStatus() {
+        isRendered = !isRendered;
+        setisRendered(isRendered);
+    }
+
+    public Identifier getTexture() {
+        Identifier texture = Custom01;
+        if (rotation == 1) {
+            texture = Custom02;
+        } else if (rotation == 2) {
+            texture = Custom03;
+        } else if (rotation == 3) {
+            texture = Custom04;
+        } else {
+            texture = Custom01;
+        }
+        return texture;
+    }
+
+    public boolean isRendered() {
+        return isRendered;
+    }
+
+    public void setisRendered(boolean rendered) {
+        isRendered = rendered;
+        COConfigs.updateConfig(new Pair<>("display.custom", isRendered));
+    }
+    public void setRotation(int r) {
+        this.rotation = r;
+    }
+}
