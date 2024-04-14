e7313b79b559345a441acbd73e4e36e4c83ca406
fragger57 <fraggerdagger57@gmail.com>
2024-04-14 19:34:36 -0400

    Moved Project Files

---
+++ src/client/java/net/fragger/creatoroverlays/client/gui/COScreen.java
@@ -0,0 +1,11 @@
+package net.fragger.creatoroverlays.client.gui;
+
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+
+public class COScreen extends CottonClientScreen {
+
+    public COScreen(GuiDescription description) {super(description);}
+}
+
+
