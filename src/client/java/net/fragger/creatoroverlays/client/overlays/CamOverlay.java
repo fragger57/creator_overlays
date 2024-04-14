7a5ed25ea9c31e7eb5a57ae27fb6b6d125348c87
fragger57 <fraggerdagger57@gmail.com>
2024-04-14 19:27:38 -0400

    Moved Project Files

---
+++ src/client/java/net/fragger/creatoroverlays/client/overlays/CamOverlay.java
@@ -0,0 +1,85 @@
+package net.fragger.creatoroverlays.client.overlays;
+
+import com.mojang.datafixers.util.Pair;
+import net.fragger.creatoroverlays.client.Overlay;
+import net.fragger.creatoroverlays.client.StaticOverlay;
+import net.fragger.creatoroverlays.creatoroverlays;
+import net.fragger.creatoroverlays.util.config.COConfigs;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.util.Identifier;
+
+import static net.fragger.creatoroverlays.event.KeyInputHandler.vvGUI;
+import static net.fragger.creatoroverlays.event.KeyInputHandler.vvOverlay;
+import static net.fragger.creatoroverlays.util.config.COConfigs.*;
+
+public class CamOverlay extends StaticOverlay implements Overlay {
+
+    private static final Identifier FaceCamLeft = new Identifier(creatoroverlays.MOD_ID, "textures/overlays/face_cam/facecamleft.png");
+    private static final Identifier FaceCamRight = new Identifier(creatoroverlays.MOD_ID, "textures/overlays/face_cam/facecamright.png");
+    private static final Identifier FaceCamLeft_White = new Identifier(creatoroverlays.MOD_ID, "textures/overlays/face_cam/facecamleft_white.png");
+    private static final Identifier FaceCamRight_White = new Identifier(creatoroverlays.MOD_ID, "textures/overlays/face_cam/facecamright_white.png");
+    private static final Identifier FaceCamLeft_Red = new Identifier(creatoroverlays.MOD_ID, "textures/overlays/face_cam/facecamleft_red.png");
+    private static final Identifier FaceCamRight_Red = new Identifier(creatoroverlays.MOD_ID, "textures/overlays/face_cam/facecamright_red.png");
+
+    private static boolean isRendered = false;
+
+    @Override
+    public void onHudRender(DrawContext drawContext, float tickDelta) {
+        if (isRendered) {
+            render(drawContext, getTexture());
+        }
+    }
+
+    public void updateRenderStatus() {
+        isRendered = !isRendered;
+        setisRendered(isRendered);
+    }
+
+    public Identifier getTexture() {
+        Identifier texture = FaceCamLeft;
+        if (state) {
+            if (color == 1) {
+                texture = FaceCamLeft_White;
+            } else if (color == 2) {
+                texture = FaceCamLeft_Red;
+            } else {
+                texture = FaceCamLeft;
+            }
+        } else {
+            if (color == 1) {
+                texture = FaceCamRight_White;
+            } else if (color == 2) {
+                texture = FaceCamRight_Red;
+            } else {
+                texture = FaceCamRight;
+            }
+        }
+        return texture;
+    }
+
+    public boolean isRendered() {
+        return isRendered;
+    }
+
+    public void setisRendered(boolean rendered) {
+        isRendered = rendered;
+        COConfigs.updateConfig(new Pair<>("display.cam", isRendered));
+        if (isRendered) {
+            if (vvOverlay.isRendered()) {
+                vvGUI.toggle();
+            }
+            if (DISPLAY_VV) {
+                COConfigs.updateConfig(new Pair<>("display.vv", false));
+            }
+        }
+        if (!DISPLAY_RO3 && !DISPLAY_VV && !DISPLAY_CAM && !DISPLAY_CUSTOM) {
+            COConfigs.updateConfig(new Pair<>("display.ro3", true));
+        }
+    }
+    public void setState(boolean s) {
+        this.state = s;
+    }
+    public void setColor(int c) {
+        this.color = c;
+    }
+}
